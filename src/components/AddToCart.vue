d0cebd3aa082e9e2427a2d9cc86afd7c2413f5e8
@thar <tharnnn1401@gmail.com>
2020-02-28 03:05:36 +0700

    2020.02.28

---
+++ src/components/AddToCart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="add-to-cart">
+   
+        <button class="btn btn-primary mr-1" @click="addToCart">Add to Cart</button>
+        <button class="btn btn-primary mr-1" @click="popup">More Details</button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "add-to-cart",
+  props: {
+    name: String,
+    price: String,
+    image: String,
+    pId: String
+  },
+
+  data(){
+      return {
+          item :{
+            productName: this.name,
+            productImage: this.image,
+            productPrice: this.price,
+            productId: this.pId,
+            productQuantity: 1,
+          }
+
+      }
+  },
+  methods:{
+    addToCart(){
+      $('#miniCart').modal('show');
+      this.$store.commit('addToCart', this.item)
+      console.log('pId',this.item);
+    },
+      popup(){
+      $('#ProductPopup').modal('show');
+      // this.$store.commit('addToCart', this.item)
+      // console.log('pId',this.item);
+    }
+
+  }
+
+};
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
